eac351c3dca10d747d5c40ee5f3c789a92034e6e
KrupoderovMikhail <mishka.crash@gmail.com>
2019-07-20 13:30:28 +0500

    Add main config and maven dependencies

--- src/main/java/com/KrupoderovMikhail/github/Main.java
+++ src/main/java/com/KrupoderovMikhail/github/Main.java
@@ -1,7 +1,27 @@
 package com.KrupoderovMikhail.github;
 
-public class Main {
-    public static void main(String[] args) {
+import lombok.extern.java.Log;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
 
+import javax.security.auth.login.LoginException;
+
+@Log
+public class Main {
+
+    private Main() {
+        try {
+            log.info("Booting...");
+            new JDABuilder(AccountType.CLIENT)
+                    .setToken(Secrets.TOKEN)
+                    .setAudioEnabled(false)
+                    .build().awaitReady();
+            log.info("Running...");
+        } catch (InterruptedException | LoginException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new Main();
     }
 }
