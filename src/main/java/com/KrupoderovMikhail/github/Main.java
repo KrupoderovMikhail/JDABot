629ff41279c0d978039865f87a3daa2da96e6c87
KrupoderovMikhail <mishka.crash@gmail.com>
2019-07-23 16:38:04 +0500

    Slightly edited code.

--- src/main/java/com/KrupoderovMikhail/github/Main.java
+++ src/main/java/com/KrupoderovMikhail/github/Main.java
@@ -1,29 +1,30 @@
 package com.KrupoderovMikhail.github;
 
+import com.KrupoderovMikhail.github.commands.fun.CatCommand;
 import com.KrupoderovMikhail.github.secrets.Secrets;
-import lombok.extern.java.Log;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
 
-@Log
 public class Main {
 
     CommandManager commandManager = new CommandManager();
     Listener listener = new Listener(commandManager);
+    private static final Logger LOGGER = LoggerFactory.getLogger(CatCommand.class);
 
     private Main() {
         try {
-            log.info("Booting");
+            LOGGER.info("Booting");
             new JDABuilder(AccountType.BOT)
                     .setToken(Secrets.TOKEN)
-                    .setAudioEnabled(false)
-                    .setGame(Game.streaming("JDA", "https://www.twitch.tv/santamas"))
+                    .setGame(Game.listening("!help"))
                     .addEventListener(listener)
                     .build().awaitReady();
-            log.info("Running");
+            LOGGER.info("Running");
         } catch (InterruptedException | LoginException e) {
             e.printStackTrace();
         }
