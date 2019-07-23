1e3e90c1a1fe39b3be342d3978c9fb43c0af243f
KrupoderovMikhail <mishka.crash@gmail.com>
2019-07-23 19:14:36 +0500

    Improved command - Play. Added information on the command (when calling "help"). Added the ability to add your song to the queue (youtube, soundcloud, bandcamp).

--- src/main/java/com/KrupoderovMikhail/github/commands/music/PlayCommand.java
+++ src/main/java/com/KrupoderovMikhail/github/commands/music/PlayCommand.java
@@ -1,26 +1,56 @@
 package com.KrupoderovMikhail.github.commands.music;
 
+import com.KrupoderovMikhail.github.Constants;
 import com.KrupoderovMikhail.github.music.PlayerManager;
 import com.KrupoderovMikhail.github.objects.ICommand;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 public class PlayCommand implements ICommand {
     @Override
     public void handle(List<String> args, GuildMessageReceivedEvent event) {
 
+        TextChannel channel = event.getChannel();
+
+        if (args.isEmpty()) {
+            channel.sendMessage("Please provide some arguments").queue();
+
+            return;
+        }
+
+        String input = String.join(" ", args);
+
+        if (!isURL(input) && input.startsWith("vtsearch:")) {
+            // Use the youtube api for search instead, making a lot of requests with "vtsearch:" will get you blocked
+            channel.sendMessage("Please provide a valid youtube, soundcloud or bandcamp link").queue();
+
+            return;
+        }
+
         PlayerManager manager = PlayerManager.getInstance();
 
-        manager.loadAndPlay(event.getChannel(), "https://www.youtube.com/watch?v=f4Mc-NYPHaQ");
+        manager.loadAndPlay(event.getChannel(), input);
 
         manager.getGuildMusicManager(event.getGuild()).player.setVolume(100);
+    }
 
+    private boolean isURL(String input) {
+        try {
+            new URL(input);
+            return true;
+        } catch (MalformedURLException ignored) {
+            return false;
+        }
     }
 
     @Override
     public String getHelp() {
-        return null;
+        return "Plays a song\n" +
+                "Usage: `" + Constants.PREFIX + getInvoke() + " <song url>`";
     }
 
     @Override
